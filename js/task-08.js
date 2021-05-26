562745a8b5c727ddb9d2d0b993faee7386ce8977
Grek-88 <73738671+Grek-88@users.noreply.github.com>
2021-05-26 11:40:36 +0300

    update task-08

--- js/task-08.js
+++ js/task-08.js
@@ -28,10 +28,18 @@ function createBoxes(amount) {
   amount = numderCreateBoxes.value;
 
   let sizeBox = 30;
-  for (let i = 0; i < amount; i += 1) {
-    const newElem = document.createElement("div");
-    newElem.style.width = `${sizeBox}px`;
-    newElem.style.height = `${sizeBox}px`;
+  if (createBoxesEl.children.length > 0) {
+    sizeBox = parseInt(createBoxesEl.lastChild.style.width) + 10;
+  }
+
+  let arrEL = [];
+  arrEL.length = Number(amount);
+
+  const arrBoxElems = [...arrEL].map((el) => {
+    el = document.createElement("div");
+
+    el.style.width = `${sizeBox}px`;
+    el.style.height = `${sizeBox}px`;
 
     function getRandomRGB(max) {
       return Math.floor(Math.random() * Math.floor(max));
@@ -41,14 +49,38 @@ function createBoxes(amount) {
     const green = getRandomRGB(255);
     const blue = getRandomRGB(255);
 
-    newElem.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    el.style.backgroundColor = `rgb(${red},${green},${blue})`;
 
-    arr.push(newElem);
+    arrEL.push(el);
 
     sizeBox += 10;
-  }
 
-  createBoxesEl.append(...arr);
+    return el;
+  });
+
+  createBoxesEl.append(...arrBoxElems);
+
+  // for (let i = 0; i < amount; i += 1) {
+  //   const newElem = document.createElement("div");
+  //   newElem.style.width = `${sizeBox}px`;
+  //   newElem.style.height = `${sizeBox}px`;
+
+  //   function getRandomRGB(max) {
+  //     return Math.floor(Math.random() * Math.floor(max));
+  //   }
+
+  //   const red = getRandomRGB(255);
+  //   const green = getRandomRGB(255);
+  //   const blue = getRandomRGB(255);
+
+  //   newElem.style.backgroundColor = `rgb(${red},${green},${blue})`;
+
+  //   arr.push(newElem);
+
+  //   sizeBox += 10;
+  // }
+
+  // createBoxesEl.append(...arr);
 }
 
 btnDestroy.addEventListener("click", destroyBoxes);
